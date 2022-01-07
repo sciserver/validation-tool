632f54f5c85817663f249e29dd11518b4483e96b
Rafael Ladislau <rafa.ladis@gmail.com>
2022-01-06 22:14:50 -0300

    Adding first and last name to JWT

--- validation-tool-backend/src/auth/auth.service.ts
+++ validation-tool-backend/src/auth/auth.service.ts
@@ -19,7 +19,12 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { email: user.email, sub: user.source_id };
+    const payload = {
+      email: user.email,
+      sub: user.source_id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+    };
     return {
       access_token: this.jwtService.sign(payload),
     };
